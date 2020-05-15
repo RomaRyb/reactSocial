5e4266241698701c272e507dd87a42b5d5a3d5b4
Roma Rub <rubashevskyiroman@gmail.com>
2020-05-15 22:24:49 +0300

    19 - route, browser-router

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,19 +2,24 @@ import React from 'react';
 import s from'./Navbar.module.css'
 console.log(s);
 
-// let s = {
-//     'nav': 'Navbar_nav__2jCdr',
-//     'item': 'Navbar_item__1odmO'
-// }
-
 
 const Navbar = () => {
     return <nav className={s.nav}>
-        <div className={s.item}><a className={`${s.item} ${s.active}`} href="#1">Profile</a></div>
-        <div className={s.item}><a href="#2">Messages</a></div>
-        <div className={s.item}><a  href="#3">News</a></div>
-        <div className={s.item}><a href="#4">Music</a></div>
-        <div className={`${s.item} ${s.active}`}><a href="#5">Settings</a></div>
+        <div className={`${s.item} ${s.active}`}>
+            <a href="/profile">Profile</a>
+        </div>
+        <div className={s.item}>
+            <a href="/dialogs">Messages</a>            
+        </div>
+        <div className={s.item}>
+            <a href="/news">News</a>
+        </div>
+        <div className={s.item}>
+            <a href="/music">Music</a>
+        </div>
+        <div className={s.item}>
+            <a href="/settings">Settings</a>
+        </div>
 </nav>
 }
 
