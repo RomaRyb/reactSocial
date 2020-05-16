b35648fe33b9c9cae34b74b18235cd3224c541b6
Roma Rub <rubashevskyiroman@gmail.com>
2020-05-16 23:13:50 +0300

    20 - NavLink

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const App = () => {
             <div className='app-wrapper'>
                 <Header />
                 <Navbar />
-                <div class='app-wrapper-content'>
+                <div className='app-wrapper-content'>
                     <Route path='/dialogs' component={Dialogs} />
                     <Route path='/profile' component={Profile} />
                     <Route path='/news' component={Profile} />
