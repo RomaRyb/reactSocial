bba61c4decbb031a3c865f638a9fa43f4ebd90be
Roma Rub <rubashevskyiroman@gmail.com>
2020-05-20 14:49:35 +0300

    27 - component vs render перекид propsс через render

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
+import Dialogs from './components/Dialogs/Dialogs';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
-import Dialogs from './components/Dialogs/Dialogs';
-import { Route, BrowserRouter } from 'react-router-dom';
 
 
 const App = () => {
@@ -14,11 +14,14 @@ const App = () => {
                 <Header />
                 <Navbar />
                 <div className='app-wrapper-content'>
-                    <Route path='/dialogs' component={Dialogs} />
-                    <Route path='/profile' component={Profile} />
-                    <Route path='/news' component={Profile} />
+                    {/* <Route path='/dialogs' component={Dialogs} />
+                    <Route path='/profile' component={Profile} /> */}
+
+                    <Route path='/dialogs' render={ () => <Dialogs/>} />
+                    <Route path='/profile' render={ () => <Profile/>} />
+                    {/* <Route path='/news' component={Profile} />
                     <Route path='/music' component={Profile} />
-                    <Route path='/settings' component={Profile} />
+                    <Route path='/settings' component={Profile} /> */}
                 </div>
             </div>
         </BrowserRouter>
