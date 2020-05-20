b2dffe33160d656a08159aafe8fa67e669a4615a
Roma Rub <rubashevskyiroman@gmail.com>
2020-05-20 12:01:19 +0300

    26 - refactoring

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,44 +1,32 @@
 import React from 'react';
+import DialogItem from './DialogItem/DialogItem';
 import s from './Dialogs.module.css';
-import { NavLink } from 'react-router-dom';
+import Message from './Message/Message';
 
-const DialogItem = (props) => {
-    let path = "/dialogs/" + props.id;
-    return (
-        <div className={`${s.dialog} ${s.active}`}>
-            <NavLink to={path}> {props.name} </NavLink>
-        </div>
-    )
-}
-
-const Message = (props) => {
-    return (
-        <div className={s.dialog}> {props.message} </div>
-    )
-}
 
 const Dialogs = (props) => {
 
     let dialogs = [
-        {id: 1, name: 'Roma'},
-        {id: 2, name: 'Fanja'},
-        {id: 3, name: 'Boltus'},
-        {id: 4, name: 'Gippik'},
-        {id: 5, name: 'Dukui'},
-        {id: 6, name: 'Dimon'}
-    ]    
+        { id: 1, name: 'Roma' },
+        { id: 2, name: 'Fanja' },
+        { id: 3, name: 'Boltus' },
+        { id: 4, name: 'Gippik' },
+        { id: 5, name: 'Dukui' },
+        { id: 6, name: 'Dimon' }
+    ]
 
     let messages = [
-        {id: 1, message: '"Yofff'},
-        {id: 2, message: 'what up'},
-        {id: 3, message: 'coooollll'},
-        {id: 4, message: 'gogogogo'},
-        {id: 5, message: 'waaaaaaiiiitt'}
+        { id: 1, message: '"Yofff' },
+        { id: 2, message: 'what up' },
+        { id: 3, message: 'coooollll' },
+        { id: 4, message: 'gogogogo' },
+        { id: 5, message: 'waaaaaaiiiitt' }
     ]
 
     let dialogsElements = dialogs.map(d => <DialogItem name={d.name} id={d.id} />);
     let messagesElements = messages.map(m => <Message message={m.message} />)
 
+    
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
