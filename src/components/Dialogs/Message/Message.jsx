d743824b8deaf2fe4fb14b116107636589e6dc83
Roma Rub <rubashevskyiroman@gmail.com>
2020-05-26 23:44:57 +0300

    onClick, ref, Virtual DOM

--- src/components/Dialogs/Message/Message.jsx
+++ src/components/Dialogs/Message/Message.jsx
@@ -2,8 +2,26 @@ import React from 'react';
 import s from './../Dialogs.module.css';
 
 const Message = (props) => {
+
+    let newMessageElement = React.createRef();
+    let addMessage = () => {
+        let text = newMessageElement.current.value;
+        alert(text);
+    }
+
+
+
     return (
-        <div className={s.dialog}> {props.message} </div>
+        <div className={s.dialog}> {props.message} 
+        
+        <div>
+            <textarea ref={newMessageElement}></textarea>
+        </div>
+        <div>
+            <button onClick={addMessage}>Add</button>
+        </div>
+        
+        </div>
     )
 }
 
