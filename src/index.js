bbadd935533ccac0d067e686a07c0b65472ca25f
Roma Rub <rubashevskyiroman@gmail.com>
2020-05-27 19:55:46 +0300

    33 -add post to he wall

--- src/index.js
+++ src/index.js
@@ -1,22 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
 import './index.css';
-import state, { addPost } from './redux/state';
+import state from './redux/state';
+import { rerenderEntireTree } from './render';
 import * as serviceWorker from './serviceWorker';
 
+rerenderEntireTree(state);
 
-ReactDOM.render(
-  <BrowserRouter>
-      <React.StrictMode>
-        <App state={state} addPost={addPost}/>
-      </React.StrictMode>
-  </BrowserRouter>,
-    document.getElementById('root')
-);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+serviceWorker.unregister(state);
---
+++ src/render.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import './index.css';
+import { addPost } from './redux/state';
+
+export let rerenderEntireTree = (state) => {
+  ReactDOM.render(
+    <BrowserRouter>
+        <React.StrictMode>
+          <App state={state} addPost={addPost}/>
+        </React.StrictMode>
+    </BrowserRouter>,
+      document.getElementById('root')
+  );
+}
+
