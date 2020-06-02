f7ee158dec90a795ad97cfd161b83f6ee48cfe53
Roma Rub <rubashevskyiroman@gmail.com>
2020-06-02 22:44:11 +0300

    45 connect, mapStateToProps, mapDispatchToProps

--- src/components/Dialogs/DialogsContainer.jsx
+++ src/components/Dialogs/DialogsContainer.jsx
@@ -1,32 +1,25 @@
 import React from 'react';
 import { sendMessageCreator, updateNewMessageBodyCreator } from '../../redux/dialogs-reducer';
 import Dialogs from './Dialogs';
-import StoreContext from '../../StoreContext';
+import { connect } from 'react-redux';
 
 
-const DialogsContainer = (props) => {
-
-    return <StoreContext.Consumer> 
-        {  (store) => {
-            
-    let state = store.getState().dialogsPage;
-
-    let onSendMessageClick = () => {
-        store.dispatch(sendMessageCreator());        
+const mapStateToProps = (state) => {
+    return {
+        dialogsPage: state.dialogsPage
     }
-    
-    let onNewMessageChange = (body) => {
-        store.dispatch(updateNewMessageBodyCreator(body));
-    }
-    
-        return    <Dialogs
-                updateNewMessageBody={onNewMessageChange}
-                sendMessage={onSendMessageClick}
-                dialogsPage={state}
-            />
+}
+const mapDispatchToProps = (dispatch) => {
+    return {
+        updateNewMessageBody: () => {
+            dispatch(sendMessageCreator());
+        },
+        sendMessage: (body) => {
+            dispatch(updateNewMessageBodyCreator(body));
         }
     }
-    </StoreContext.Consumer>
 }
+
+const DialogsContainer = connect(mapStateToProps, mapDispatchToProps) (Dialogs);
 
 export default DialogsContainer;
--- src/index.js
+++ src/index.js
@@ -4,20 +4,19 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 import store from './redux/redux-store';
-import StoreContext from './StoreContext';
+import { Provider } from 'react-redux';
 
-let rerenderEntireTree = (state) => {
-        
+let rerenderEntireTree = () => {
         ReactDOM.render(
-        <BrowserRouter>
-                <StoreContext.Provider value={store}>
-                        <App />     
-                </StoreContext.Provider>       
-        </BrowserRouter>,document.getElementById('root'));
+                <BrowserRouter>
+                        <Provider store={store}>
+                                <App />
+                        </Provider>
+                </BrowserRouter>, document.getElementById('root'));
 }
 
-rerenderEntireTree(store.getState ());
-store.subscribe( () =>{
-        let state = store.getState ();
-        rerenderEntireTree(state);
+rerenderEntireTree();
+
+store.subscribe(() => {
+        rerenderEntireTree();
 });
