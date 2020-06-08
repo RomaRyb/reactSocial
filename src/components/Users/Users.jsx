e711bf8f61df74cbea5d80454cf1fc6fdd697118
Roma Rub <rubashevskyiroman@gmail.com>
2020-06-08 22:11:49 +0300

    51 - users API

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,24 +1,16 @@
 import React from 'react';
 import styles from './Users.module.css';
+import * as axios from 'axios';
+import userPhoto from '../../assets/images/user.png'
 
 let Users = (props) => {
 
     if (props.users.length === 0) {
-        props.setUsers([
-            {
-                id: 1, photoUrl: 'https://image.winudf.com/v2/image/Y29tLkZyZWVHYW1lc1Byb2QuU2FzdWtlVWNoaWhhX3NjcmVlbl8wXzE1MDU0MTc1MTRfMDM2/screen-0.jpg?fakeurl=1&type=.jpg',
-                followed: false, fullName: 'Roma.R', status: 'want a gob', location: { country: 'Ukraine', city: 'Lviv' }
-            },
-            {
-                id: 2, photoUrl: 'https://image.winudf.com/v2/image/Y29tLkZyZWVHYW1lc1Byb2QuU2FzdWtlVWNoaWhhX3NjcmVlbl8wXzE1MDU0MTc1MTRfMDM2/screen-0.jpg?fakeurl=1&type=.jpg',
-                followed: true, fullName: 'Sasha', status: 'want a gob too', location: { country: 'Ukraine', city: 'Kyiv' }
-            },
-            {
-                id: 3, photoUrl: 'https://image.winudf.com/v2/image/Y29tLkZyZWVHYW1lc1Byb2QuU2FzdWtlVWNoaWhhX3NjcmVlbl8wXzE1MDU0MTc1MTRfMDM2/screen-0.jpg?fakeurl=1&type=.jpg',
-                followed: false, fullName: 'Vita', status: 'and i want a gob', location: { country: 'Ukraine', city: 'Chervonograd' }
-            },
-    
-        ])
+        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {            
+            props.setUsers(response.data.items);
+        })
+
+        
     }    
 
     return <div>
@@ -26,7 +18,7 @@ let Users = (props) => {
             props.users.map(u => <div key={u.id}>
                 <span>
                     <div>
-                        <img src={u.photoUrl} alt={''} className={styles.userPhoto} />
+                        <img src={u.photos.small != null ? u.photos.small : userPhoto} alt={''} className={styles.userPhoto} />
                     </div>
                     <div>
                         {u.followed ? <button onClick={() => { props.unfollow(u.id) }} >Unfollow</button> :
@@ -34,12 +26,12 @@ let Users = (props) => {
                     </div>
                 </span>
                 <span>
-                    <div> {u.fullName} </div>
+                    <div> {u.name} </div>
                     <div> {u.status} </div>
                 </span>
                 <span>
-                    <div> {u.location.country} </div>
-                    <div> {u.location.city} </div>
+                    <div> {'u.location.country'} </div>
+                    <div> {'u.location.city'} </div>
                 </span>
             </div>)
 
