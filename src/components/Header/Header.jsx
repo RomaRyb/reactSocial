15a58a98e80d0870e9d072bb4c74f925dbdc7ca0
Roma Rub <rubashevskyiroman@gmail.com>
2020-06-15 23:51:59 +0300

    61 start

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import s from './Header.module.css'
+import { NavLink } from 'react-router-dom';
 
 const Header = () => {
     return <header className={s.header}>
-    <div className="header"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSSV8UX3k6Gxukve6ZWXLdAksSAp2hjYiXfzceHdDwsdVr3eMk8&usqp=CAU" alt="" /></div>
-</header>
+        <div className="header">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSSV8UX3k6Gxukve6ZWXLdAksSAp2hjYiXfzceHdDwsdVr3eMk8&usqp=CAU" alt="" />
+            <div className={s.loginBlock} >
+                <NavLink to={'/login'}>Login</NavLink>
+            </div>
+        </div>
+    </header>
 }
 
 export default Header;
